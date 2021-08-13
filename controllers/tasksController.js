389e3a0a1840f7adc526ed0c831ea495e7d141f9
Pablo Fiorido <pablofiorido@hotmail.com>
2021-08-12 21:17:40 -0300

    controller modificada

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -3,18 +3,69 @@ const handleError = require("../helpers/errorHandler");
 const taskModel = require("../models/tasks");
 
 const controller = {
+
+
+
     getAllTaks: () => {
         return actionHandler(() => taskModel.find());
     },
+
+    addTask: (data) => 
+    new Promise((resolve, reject) => {
+
+        
+
+        return actionHandler(async()  => {
+            const result = await taskModel.create(data)
+            resolve(result);
+        });
+    }),
+
+
+
+    /*
     addTask: (data) => {
         return actionHandler(() => taskModel.create(data));
     },
+*/
+
+
+
+
+/*
+
     removeTask: (id) => {
         return actionHandler(() => taskModel.deleteOne({ _id: id }));
     },
+
+    */
+
+    removeTask: (id) => 
+    new Promise((resolve, reject) => {
+        return actionHandler(async()  => {
+            const result = await taskModel.deleteOne({ _id: id });
+            resolve(result);
+        });
+    }),
+
+
+    
+
+    editTask: (data) => 
+    new Promise((resolve, reject) => {
+        return actionHandler(async()  => {
+            const result = await taskModel.findOneAndUpdate({ _id: data._id }, data, { new: true, useFindAndModify: false }, handleError);
+            resolve(result);
+        });
+    }),
+
+/*
     editTask: (data) => {
         return actionHandler(() => taskModel.findOneAndUpdate({ _id: data._id }, data, { new: true, useFindAndModify: false }, handleError));
     }
+
+    */
+
 }
 
 module.exports = controller;
